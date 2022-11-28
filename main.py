8aff8be44db897ed67fafb593428577ff558dba2
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2022-11-28 16:36:24 +0900

    リズム生成

--- main.py
+++ main.py
@@ -9,21 +9,45 @@ def nextchord(prev):
         next = [2, 4, 5, 5]
     if prev == 5:
         next = [1, 1, 3, 3, 6, 6, 5]
-    return next[int(len(next) * random.random())]
-
+    return random.choice(next)
 
 def writechord(c):
     print(f"A {'_cdefga'[c]}1")
     print(f"1A {'_efgabc'[c]}1")
     print(f"2A {'_gabcde'[c]}1")
 
-
 chords = []
 while True:
-    chords = [int(1 + 6 * random.random())]
+    chords = [random.choice(range(1, 7))]
     for i in range(4):
         chords.append(nextchord(chords[-1]))
     if chords[4] == chords[0]:
         break
 for c in chords:
     writechord(c)
+
+rhythmpattern_8beat = [
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 0, 1, 0, 0],
+    [1, 0, 0, 1, 0, 0, 1, 0],
+    [1, 0, 0, 1, 0, 1, 0, 0],
+]
+
+rp_base = random.choice(rhythmpattern_8beat)
+for i in range(4):
+    rp_melody = []
+    for j in range(8):
+        if j == 0:
+            p = 1
+        elif rp_base[j]:
+            p = 7 / 8
+        else:
+            p = 1 / 4
+        rp_melody.append(random.random() <= p)
+    mml_out = "B C10l8J36"
+    for j in range(8):
+        if rp_melody[j]:
+            mml_out += "c"
+        else:
+            mml_out += "r"
+    print(mml_out)
