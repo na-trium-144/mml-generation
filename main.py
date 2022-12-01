6c19ceb6c18ceadcca48f9b1945e793ce9aa5038
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2022-12-02 01:24:22 +0900

    メロディー生成

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import random
+import sys
 
+# print("A t150")
 
 def nextchord(prev):
     if prev == 1 or prev == 3 or prev == 6:
@@ -26,12 +28,32 @@ while True:
 for c in chords:
     writechord(c)
 
+def nextnote(prev, chord):
+    next_pr = [0] * 7
+    next_pr[prev % 7] = 2
+    for i in range(1, 4):
+        next_pr[(prev + i) % 7] = 4 - i
+        next_pr[(prev + 7 - i) % 7] = 4 - i
+    for i in range(3):
+        next_pr[(chord + i * 2) % 7] += 1
+    next = []
+    for i in range(7):
+        next += [i] * int(next_pr[i])
+    # sys.stderr.write(f"{prev}, {next_pr}\n")
+    n = random.choice(next)
+    if n == 0:
+        n = 7
+    return n
+
 rhythmpattern_8beat = [
     [1, 0, 1, 0, 1, 0, 1, 0],
     [1, 0, 1, 0, 0, 1, 0, 0],
     [1, 0, 0, 1, 0, 0, 1, 0],
     [1, 0, 0, 1, 0, 1, 0, 0],
 ]
+
+note = random.choice(range(1, 8))
+prev_note = 4
 
 rp_base = random.choice(rhythmpattern_8beat)
 for i in range(4):
@@ -44,10 +66,17 @@ for i in range(4):
         else:
             p = 1 / 4
         rp_melody.append(random.random() <= p)
-    mml_out = "B C10l8J36"
+    mml_out = "B C2l8"
+
     for j in range(8):
         if rp_melody[j]:
-            mml_out += "c"
+            note = nextnote(note, chords[i])
+            if note - prev_note >= 4:
+                mml_out += "<" #オクターブ下げ
+            if note - prev_note <= -4:
+                mml_out += ">" #オクターブ上げ
+            mml_out += "_cdefgab"[note]
+            prev_note = note
         else:
-            mml_out += "r"
+            mml_out += "^"
     print(mml_out)
