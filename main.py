ed009b13105d3105fef689c899e7334b02d74092
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2022-12-11 16:22:17 +0900

    1曲完成?

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 import random
 import sys
 
-# print("A t150")
+print("A t" + str(random.choice(range(120, 160, 5))))
+print("A C1o3@4")
+print("B C2o5@0")
 
 
 def nextchord(prev):
@@ -15,30 +17,20 @@ def nextchord(prev):
     return random.choice(next)
 
 
-flip = random.choice([True, False])
-def writechord(c, rp_base):
+def writechord(c, rp_base, flip):
     mml_out = ["", "", ""]
     for b in rp_base:
         if b ^ flip:
-            mml_out[0] += '_cdecdc'[c]
-            mml_out[1] += 'r'
-            mml_out[2] += 'r'
+            mml_out[0] += "_cdecdc"[c]
+            mml_out[1] += "r"
+            mml_out[2] += "r"
         else:
-            mml_out[0] += 'r'
-            mml_out[1] += '_efgfge'[c]
-            mml_out[2] += '_gababa'[c]
+            mml_out[0] += "r"
+            mml_out[1] += "_efgfge"[c]
+            mml_out[2] += "_gababa"[c]
     print(f"A l8{mml_out[0]}")
     print(f"1A l8{mml_out[1]}")
     print(f"2A l8{mml_out[2]}")
-
-
-chords = []
-while True:
-    chords = [random.choice(range(1, 7))]
-    for i in range(4):
-        chords.append(nextchord(chords[-1]))
-    if chords[4] == chords[0]:
-        break
 
 
 def randompick(pr):
@@ -84,61 +76,89 @@ def nextnote_beat(prev, chord):
     return n
 
 
-rhythmpattern_8beat = [
-    [1, 0, 1, 0, 1, 0, 1, 0],
-    [1, 0, 1, 0, 0, 1, 0, 0],
-    [1, 0, 0, 1, 0, 0, 1, 0],
-    [1, 0, 0, 1, 0, 1, 0, 0],
-]
+start_chord = 1
+for p in range(2):
+    chords = []
+    while True:
+        chords = [start_chord]
+        for i in range(4):
+            chords.append(nextchord(chords[-1]))
+        if chords[4] == chords[0]:
+            break
+    del chords[4]
+    start_chord = chords[3]
 
-note = random.choice(range(1, 8))
-prev_note = 4
+    rhythmpattern_8beat = [
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [1, 0, 1, 0, 0, 1, 0, 0],
+        [1, 0, 0, 1, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0, 1, 0, 0],
+    ]
 
-rp_base = random.choice(rhythmpattern_8beat)
-for c in chords:
-    writechord(c, rp_base)
+    rp_base = random.choice(rhythmpattern_8beat)
+    flip = random.choice([True, False])
 
-for i in range(4):
-    rp_melody = []
-    for j in range(8):
-        if j == 0:
-            p = 1
-        elif rp_base[j]:
-            p = 7 / 8
-        else:
-            p = 1 / 4
-        rp_melody.append(random.random() <= p)
-    mml_out = "B C2l8"
+    for l in range(2):
+        note = random.choice(range(1, 8))
+        prev_note = 4
+        print("B o5l8")
 
-    for j in range(8):
-        if rp_melody[j]:
-            if j == 0:
-                note = nextnote_first(note, chords[i])
-            elif rp_base[j]:
-                note = nextnote_beat(note, chords[i])
-            else:
-                note = nextnote_other(note, chords[i])
-            if note - prev_note >= 4:
-                mml_out += "<"  # オクターブ下げ
-            if note - prev_note <= -4:
-                mml_out += ">"  # オクターブ上げ
-            mml_out += "_cdefgab"[note]
-            prev_note = note
-        else:
-            mml_out += "^"
-    print(mml_out)
+        for c in chords:
+            writechord(c, rp_base, flip)
 
-mml_out = "c"
-for j in range(7):
-    mml_out += random.choice("ccr")
-print("C C10l8J36" + mml_out * 4)
-print("D C10l4J40 [rcrc]4")
-# mml_out = ""
-# for j in range(8):
-#     mml_out += random.choice("crr")
-# print("D C10l8J40" + mml_out * 4)
-mml_out = ""
-for j in range(4):
-    mml_out += "J44" + random.choice(["cc", "cr", "cr", "rr"])
-    mml_out += "J42" + random.choice(["cc", "cr", "cr", "rr"])
-print("E C10l16" + mml_out * 4)
+        for i in range(4):
+            rp_melody = []
+            for j in range(8):
+                if j == 0:
+                    p = 1
+                elif rp_base[j]:
+                    p = 7 / 8
+                else:
+                    p = 1 / 4
+                rp_melody.append(random.random() <= p)
+            mml_out = "B "
+
+            for j in range(8):
+                if rp_melody[j]:
+                    if j == 0:
+                        note = nextnote_first(note, chords[i])
+                    elif rp_base[j]:
+                        note = nextnote_beat(note, chords[i])
+                    else:
+                        note = nextnote_other(note, chords[i])
+                    if note - prev_note >= 4:
+                        mml_out += "<"  # オクターブ下げ
+                    if note - prev_note <= -4:
+                        mml_out += ">"  # オクターブ上げ
+                    mml_out += "_cdefgab"[note]
+                    prev_note = note
+                else:
+                    mml_out += "^"
+            print(mml_out)
+
+    mml_out = "c"
+    for j in range(7):
+        mml_out += random.choice("ccr")
+    print("C C10l8J36" + mml_out * 8)
+    print("D C10l4J40 [rcrc]8")
+    # mml_out = ""
+    # for j in range(8):
+    #     mml_out += random.choice("crr")
+    # print("D C10l8J40" + mml_out * 8)
+    mml_out = ""
+    for j in range(4):
+        mml_out += "J44" + random.choice(["cc", "cr", "cr", "rr"])
+        mml_out += "J42" + random.choice(["cc", "cr", "cr", "rr"])
+    print("E C10l16" + mml_out * 8)
+
+# 適当にアウトロつけてやろ
+print("A l1c")
+print("1A l1e")
+print("2A l1g")
+mml_out = "B "
+note = nextnote_first(note, 1)
+if note - prev_note >= 4:
+    mml_out += "<"  # オクターブ下げ
+if note - prev_note <= -4:
+    mml_out += ">"  # オクターブ上げ
+print(f"B l1{note}")
